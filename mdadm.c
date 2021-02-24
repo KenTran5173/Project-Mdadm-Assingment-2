c34b97416f4613f579ed8f101d221a51d7878bc4
Ben <yubenjamin0820@gmail.com>
2021-02-24 11:18:35 -0800

    working on mdadm_read

--- mdadm.c
+++ mdadm.c
@@ -17,13 +17,19 @@ void translate_address(uint32_t linear_address,
 		       int *disk_num,
 		       int *block_num,
 		       int *offset)
-{int block_remainder;
- 
+{
+  int block_remainder;
   *disk_num = linear_address / JBOD_DISK_SIZE;
   block_remainder = linear_address % JBOD_DISK_SIZE;
   *block_num = block_remainder / JBOD_BLOCK_SIZE;
   *offset = block_remainder % JBOD_BLOCK_SIZE;
   
+}
+
+int seek(int disk_num, int block_num)
+{
+  encode_operation(JBOD_SEEK_TO_DISK, disk_num, 0);
+  encode_operation(JBOD_SEEK_TO_BLOCK, 0, block_num);
 }
 
 int mdadm_mount(void) {
@@ -48,5 +54,18 @@ int mdadm_unmount(void) {
 }
 
 int mdadm_read(uint32_t addr, uint32_t len, uint8_t *buf) {
+  
+  int disk_num, block_num, offset;
+  
+  translate_address(addr, &disk_num, &block_num, &offset);
+  seek(disk_num, block_num);
+  uint32_t op = encode_operation(JBOD_READ_BLOCK, 0, 0);
+  uint32_t buf1[]; //store data into buf1 dont know how to store it
+  jbod_operation(op, buf); //[0-255]
+  //buf1 will contain data read from the disk
+  //copy buf1 to buf
+  jbod_operation(op, buf); //[256-511] until read the whole 1024
+  //append buf1 to buf
+  //use loop
   return len;
 }
