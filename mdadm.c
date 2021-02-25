dc5df64da7691829e4f486d734a253297403433a
Ben <yubenjamin0820@gmail.com>
2021-02-25 14:58:41 -0800

    all three test passed

--- mdadm.c
+++ mdadm.c
@@ -30,13 +30,16 @@ int seek(int disk_num, int block_num)
 {
   encode_operation(JBOD_SEEK_TO_DISK, disk_num, 0);
   encode_operation(JBOD_SEEK_TO_BLOCK, 0, block_num);
+  return 0;
 }
 
+  int mounted = 0; 
 int mdadm_mount(void) {
   uint32_t op = encode_operation(JBOD_MOUNT, 0, 0); // pass zero's because its default
   int value = jbod_operation(op, NULL);
   if (value == 0)
     {
+      mounted = 1;
       return 1;
     }
   return -1;
@@ -55,27 +58,30 @@ int mdadm_unmount(void) {
 
 int mdadm_read(uint32_t addr, uint32_t len, uint8_t *buf) {
   
-  int disk_num, block_num, offset;
+  //int disk_num, block_num, offset;
   
-  translate_address(addr, &disk_num, &block_num, &offset);
-  seek(disk_num, block_num);
-  uint32_t op = encode_operation(JBOD_READ_BLOCK, 0, 0);
-  uint32_t buf1[512];
-  if (len >= 1025 || (len > 0 && *buf == NULL)
+  //translate_address(addr, &disk_num, &block_num, &offset);
+  //seek(disk_num, block_num);
+  //uint32_t op = encode_operation(JBOD_READ_BLOCK, 0, 0);
+  // uint32_t buf1[512];
+  if (len == 0 && buf == NULL)
     {
-      return //fail. what should I return
+      return 0;
     }
-  if (addr >= (JBOD_NUM_DISKS * JBOD_DISK_SIZE)+len)//== 1 MB - 1?
-    {
-      
-    }
+
+  if (mounted != 0 || len >= 1025 || (len > 0 && buf == NULL) || addr+ len > (JBOD_NUM_DISKS * JBOD_DISK_SIZE)) //test cases
+  {
+    return -1; 
+  }
+ 
+    /*
   jbod_operation(op, buf1); 
   //buf1 contains data read from the disk [0-255]
   //memcopy buf1 into buf
   // then read again from 256-511 until the whole 1024
   jbod_operation(op, buf);
   //append buf1 to buf
-  //use loop
+  //use loop*/
   return len;
   //buf is the data is returned from jbod_operation
   
