d59bb25f58fbe0bf9f6642affd4474fb34a08d43
Ben <yubenjamin0820@gmail.com>
2021-03-04 13:59:02 -0800

    passed read across blocks

--- mdadm.c
+++ mdadm.c
@@ -18,7 +18,7 @@ void translate_address(uint32_t linear_address,
 		       int *block_num,
 		       int *offset)
 {
-  int block_remainder;
+  int block_remainder=0;
   *disk_num = linear_address / JBOD_DISK_SIZE;
   block_remainder = linear_address % JBOD_DISK_SIZE;
   *block_num = block_remainder / JBOD_BLOCK_SIZE;
@@ -31,6 +31,21 @@ int seek(int disk_num, int block_num)
   encode_operation(JBOD_SEEK_TO_DISK, disk_num, 0);
   encode_operation(JBOD_SEEK_TO_BLOCK, 0, block_num);
   return 1;
+}
+int min(int n, int n2)
+{
+  if (n>n2)
+    {
+      return n2;
+    }
+  else if (n==n2)
+    {
+      return n;
+    }
+  else
+    {
+      return n;
+    }
 }
 
 int mounted = 0; 
@@ -65,26 +80,44 @@ int mdadm_read(uint32_t addr, uint32_t len, uint8_t *buf) {
       return 0;
       }
   
-  if (mounted ==0 || len >= 1025 || (len > 0 && buf == NULL) || addr+ len > (JBOD_NUM_DISKS * JBOD_DISK_SIZE)) //test cases
+  if (mounted ==0 || len > 1024 || (len > 0 && buf == NULL) || addr+ len > (JBOD_NUM_DISKS * JBOD_DISK_SIZE)) //test cases
   {
     return -1; 
   }
+  int x = len;
+  int data_read=0;
+  int counter = 0;
+  int curr_addy = addr;
+  while (x!=0){
+    translate_address(curr_addy, &disk_num, &block_num, &offset);
+    seek(disk_num, block_num);
+    uint32_t op = encode_operation(JBOD_READ_BLOCK, 0, 0);
+    uint8_t buf1[256];
+    jbod_operation(op, buf1);
+    
+   if (counter == 0) //first block bc current block is equal to first block num             
+    {
+      printf("first block: %d, %d\n", data_read, offset);
+      memcpy(buf+data_read, buf1+offset, min((JBOD_BLOCK_SIZE - offset),len));
+      counter += 1; //increments the counter so not in first block anymore                      
+    }
+   else if (x < JBOD_BLOCK_SIZE)//last block, read whats left of the data by doing len subtract all the data read so far}
+    {
+      //offset = 0;
+      printf("second block: %d, %d\n", data_read, offset);
+      memcpy(buf+data_read, buf1+offset, x);
+    }
+  else
+    {
+      //offset = 0;
+      printf("third block: %d, %d\n", data_read, offset);
+      memcpy(buf+data_read, buf1, JBOD_BLOCK_SIZE); //middle blocks, so read full block size. offset is zero, new block
+    }
+   //in new blpck, but offset still the same
+     data_read = JBOD_BLOCK_SIZE - offset;//sets y to what was read so we can add it to buf
+     x = x - min(x,data_read);//decrements length
+     curr_addy = curr_addy + (JBOD_BLOCK_SIZE-offset);
+  }
   
-  translate_address(addr, &disk_num, &block_num, &offset);
-  seek(disk_num, block_num);
-  uint32_t op = encode_operation(JBOD_READ_BLOCK, 0, 0);
-  uint8_t buf1[256];
-  jbod_operation(op, buf1);
-  memcpy(buf, buf1,len); //copy the len of buf 1 into buf
-  //buf1 contains data read from the disk [0-255]
-  //memcopy buf1 into buf
-  // then read again from 256-511 until the whole 1024
-  jbod_operation(op, buf1);
-  
-  
-  //append buf1 to buf
-  //use loop*/
   return len;
-  //buf is the data is returned from jbod_operation
-  
 }
